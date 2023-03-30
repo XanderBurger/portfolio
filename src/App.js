a3924157cbbbce374d1e6e7f68d6d21705924e83
Xander Burger <xanburger@hotmail.com>
2023-03-29 23:41:51 -0600

    set up card animation

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import Card from "./Card";
 import Content from "./Content";
 import {useEffect, useRef, useState} from "react"
 
+const projectContent = <Content/>
+
 function App() {
-  const [isShow, setIsShow] = useState([false, false, false, false])
-  
+  const [isShow, setIsShow] = useState([false, false, false, false, true])
+
   function handleShow(i){
      const newList = [false, false, false, false]
      newList[i] = true
@@ -14,12 +16,11 @@ function App() {
   return (
    
     <div className="App">
-      <div onClick= {() => {setIsShow(handleShow(0))}}>
-        <Card color={"#2C9B73"} show={isShow[0]} content={Content} tile={"tester bar"} />
-      </div>
-      <Card color={"#5377D2"} show={isShow[1]} content={Content} tile={"tester bar"}/>
-      <Card color={"#DBC821"} show={isShow[2]} content={Content} tile={"tester bar"}/>
-      <Card color={"#CF4F47"} show={isShow[3]} content={Content} tile={"tester bar"}/>
+      <Card color={"#2C9B73"} show={isShow[0]} setIsShow={setIsShow} handleShow = {handleShow} i={0} content={projectContent} tile={"tester bar"}/>
+      <Card color={"#5377D2"} show={isShow[1]} setIsShow={setIsShow} handleShow = {handleShow} i={1} content={projectContent} tile={"tester bar"}/>
+      <Card color={"#DBC821"} show={isShow[2]} setIsShow={setIsShow} handleShow = {handleShow} i={2} content={projectContent} tile={"tester bar"}/>
+      <Card color={"#CF4F47"} show={isShow[3]} setIsShow={setIsShow} handleShow = {handleShow} i={3} content={projectContent} tile={"tester bar"}/>
+      <Card color={"##0F0F0E"} show={isShow[4]} setIsShow={setIsShow} handleShow = {handleShow} i={4} content={projectContent} tile={"tester bar"}/>
     </div>
   );
 }
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,23 +1,24 @@
 import React, { useState } from 'react'
 import {useSpring, animated} from "react-spring"
 
-export default function Card({color, show, content, tile}) {
+export default function Card({color, show, setIsShow, handleShow, i, content, tile}) {
     
     const animatedDiv = useSpring({
         width: show ? "82%": "4.5%",
         backgroundColor: color
     })
 
-    const animatedH2= useSpring({
+    const animatedH2 = useSpring({
         fontSize: 15,
         paddingTop: 63,
         lineHeight: 2.5,
-        fontWeight: 900
+        fontWeight: 900,
     })
 
     return (
-        <animated.div className='card' style={animatedDiv} >
-            <animated.h2 className='title' style={animatedH2}>Project Name</animated.h2>
+        <animated.div className='card' style={animatedDiv} onClick={() => (setIsShow(handleShow(i)))}>
+            <animated.h2 className='title' style={animatedH2}>{tile}</animated.h2>
+            {show && content}
         </animated.div>
     )
 }
