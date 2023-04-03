44d92631a7ebd7f9784efc0637556ea6e3c7d3e6
Xander Burger <xanburger@hotmail.com>
2023-04-03 13:10:46 -0600

    fixed content scroll

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ const projectContent = <Content/>
 function App() {
   const [isShow, setIsShow] = useState([false, false, false, false, true])
 
+  const black = "#0F0F0E"
+  const red = "#ff483a"
+  const yellow = "#ffe928"
+  const blue = "#416fe3"
+  const green ="#2cb482"
+
+
   function handleShow(i){
      const newList = [false, false, false, false]
      newList[i] = true
@@ -16,11 +23,11 @@ function App() {
   return (
    
     <div className="App">
-      <Card color={"#2cb482"} show={isShow[0]} setIsShow={setIsShow} handleShow = {handleShow} i={0} content={projectContent} tile={"Project 4"} titleColor={"#0F0F0E"}/>
-      <Card color={"#416fe3"} show={isShow[1]} setIsShow={setIsShow} handleShow = {handleShow} i={1} content={projectContent} tile={"Project 3"} titleColor={"#0F0F0E"}/>
-      <Card color={"#ffe928"} show={isShow[2]} setIsShow={setIsShow} handleShow = {handleShow} i={2} content={projectContent} tile={"Project 2"} titleColor={"#0F0F0E"}/>
-      <Card color={"#ff483a"} show={isShow[3]} setIsShow={setIsShow} handleShow = {handleShow} i={3} content={projectContent} tile={"Project 1"} titleColor={"#0F0F0E"}/>
-      <Card color={"#0F0F0E"} show={isShow[4]} setIsShow={setIsShow} handleShow = {handleShow} i={4} content={projectContent} tile={"About Me"}  titleColor={"white"}/>
+      <Card color={green} show={isShow[0]} setIsShow={setIsShow} handleShow = {handleShow} i={0} content={projectContent} tile={"Project 4"} titleColor={black}/>
+      <Card color={blue} show={isShow[1]} setIsShow={setIsShow} handleShow = {handleShow} i={1} content={projectContent} tile={"Project 3"} titleColor={black}/>
+      <Card color={yellow} show={isShow[2]} setIsShow={setIsShow} handleShow = {handleShow} i={2} content={projectContent} tile={"Project 2"} titleColor={black}/>
+      <Card color={red} show={isShow[3]} setIsShow={setIsShow} handleShow = {handleShow} i={3} content={projectContent} tile={"Project 1"} titleColor={black}/>
+      <Card color={black} show={isShow[4]} setIsShow={setIsShow} handleShow = {handleShow} i={4} content={projectContent} tile={"About Me"}  titleColor={"white"}/>
     </div>
   );
 }
