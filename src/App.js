7f3bbe084d74dc9d22de53d326c574a72111ed73
Xander Burger <xanburger@hotmail.com>
2023-04-15 21:15:15 -0600

    contnent added to sev page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Card from "./sideCard/index";
 import AboutMe from "./aboutMe/index";
+import SevProject from "./sevProject/index";
 import {useEffect, useRef, useState} from "react"
 
-const projectContent = <AboutMe/>
+const aboutMe = <AboutMe/>
+const sevProject = <SevProject/>
 
 function App() {
   const [isShow, setIsShow] = useState([false, false, false, false, true])
@@ -24,11 +26,11 @@ function App() {
   return (
    
     <div className="App">
-      <Card color={green} show={isShow[0]} setIsShow={setIsShow} handleShow = {handleShow} i={0} content={projectContent} tile={"PROJECT 4"} titleColor={black}/>
-      <Card color={blue} show={isShow[1]} setIsShow={setIsShow} handleShow = {handleShow} i={1} content={projectContent} tile={"SEVILLETA–DIGITIZED LANDSCAPE"} titleColor={black}/>
-      <Card color={yellow} show={isShow[2]} setIsShow={setIsShow} handleShow = {handleShow} i={2} content={projectContent} tile={"PROJECT 2"} titleColor={black}/>
-      <Card color={red} show={isShow[3]} setIsShow={setIsShow} handleShow = {handleShow} i={3} content={projectContent} tile={"PROJECT 1"} titleColor={black}/>
-      <Card color={cream} show={isShow[4]} setIsShow={setIsShow} handleShow = {handleShow} i={4} content={projectContent} tile={"ABOUT ME"}  titleColor={black}/>
+      <Card color={green} show={isShow[0]} setIsShow={setIsShow} handleShow = {handleShow} i={0} content={aboutMe} tile={"PROJECT 4"} titleColor={black}/>
+      <Card color={blue} show={isShow[1]} setIsShow={setIsShow} handleShow = {handleShow} i={1} content={sevProject} tile={"SEVILLETA–DIGITIZED LANDSCAPE"} titleColor={black}/>
+      <Card color={yellow} show={isShow[2]} setIsShow={setIsShow} handleShow = {handleShow} i={2} content={aboutMe} tile={"PROJECT 2"} titleColor={black}/>
+      <Card color={red} show={isShow[3]} setIsShow={setIsShow} handleShow = {handleShow} i={3} content={aboutMe} tile={"PROJECT 1"} titleColor={black}/>
+      <Card color={cream} show={isShow[4]} setIsShow={setIsShow} handleShow = {handleShow} i={4} content={aboutMe} tile={"ABOUT ME"}  titleColor={black}/>
     </div>
   );
 }
--- src/sevProject/index.jsx
+++ src/sevProject/index.jsx
@@ -1,9 +1,37 @@
 import "./index.css"
-
 import React from 'react'
+import mainPhoto from "./assets/SevProjectCorrectedBright.png"
+import sketch1 from "./assets/sevSketch1.png"
+import sketch2 from "./assets/sevSketch2.png"
+import sketch3 from "./assets/sevSketch3.png"
+import equipment1 from "./assets/PXL_20210615_163539239.jpg"
+import equipment2 from "./assets/PXL_20210615_170914779.jpg"
+import closeUp from "./assets/PXL_20210801_011300542.jpg"
 
 export default function SevProject() {
   return (
-    <div>index</div>
+    <div className="sevBody">
+        <p className="introText">
+            A project done in collaboration with the Sevilleta National Wildlife Refuge about the collection and transfer of digital ecological data.
+        </p>
+        <div className="finishedImage">
+            <img src={mainPhoto} alt="finished artwork" />
+            <p className="subHead"><b>"Digitized Landscape"</b><br/>60"x48"–drawing on paper & screen print </p>
+        </div>
+        <div className="sketches">
+            <img src={sketch1} alt="sketch" />
+            <img src={sketch2} alt="sketch" />
+            <img src={sketch3} alt="sketch" />
+            <p className="subHead"><b>"Data"</b><br/>16"x10"–drawings on paper </p>
+        </div>
+        <p className="midText">
+        </p>
+        <div className="landscapeImages">
+            <img src={equipment1} alt="equipment in desert" />
+            <img src={equipment2} alt="equipment in desert" />
+            <img src={closeUp} alt="artwork close up" />
+            <p className="subHead">subhead</p>
+        </div>
+    </div>
   )
 }
